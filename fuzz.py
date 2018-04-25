c9c8dd13b72a50114f5817530bb3a2f9ed9a6371
Baptiste MOINE <contact@bmoine.fr>
2018-04-25 17:35:47 +0200

    Fix race condition in threads and cleanup code

--- fuzz.py
+++ fuzz.py
@@ -1,98 +1,173 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import argparse
 import traceback
-import requests
+import threading
+import argparse
+import logging
+import copy
 import sys
 import re
-import sys
-import codecs
 import io
 
 is_py2 = sys.version[0] == '2'
 if is_py2:
-    from Queue import Queue
     import httplib
     from urlparse import urlparse
 else:
-    from queue import Queue
     import http.client as httplib
     from urllib.parse import urlparse
 
-from threading import Thread
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-
 DEBUG = False
 STANDARD_UA = 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+class Wordlist(object):
+    """Simple thread-safe wordlist iterator."""
+    def __init__(self, wordlist_path):
+        try:
+            self.wordlist = io.FileIO(wordlist_path, mode='r')
+        except IOError as exception_:
+            raise IOError("Error opening file. {}".format(exception_))
+        else:
+            self.wordlist.seek(0)
+            self._opened = True
+
+    def next(self):
+        return (next(self.wordlist)).decode('utf-8').strip()
+
+    def __next__(self):
+        return self.next()
+
+    def __iter__(self):
+        return self
+
+    def close(self):
+        self.wordlist.close()
+        self._opened = False
 
 class UAFuzz(object):
     """Perform UA fuzzing."""
-    def __init__(self, index, queue, url, headers, standard_size, standard_source):
+    def __init__(self, index, wordlist, url, headers, standard_size, standard_source):
         self.index = index
-        self.queue = queue
+        self.wordlist = wordlist
         self.url = url
         self.headers = headers
         self.standard_size = standard_size
         self.standard_source = standard_source
+        self.stop = False
+        self.user_agent = headers['User-Agent']
 
     def run(self):
-        while True:
-            #get your data off the queue, and do some work
-            self.user_agent = str(self.queue.get(block=True))
+        # While there is UA left and we're not stopping
+        while self.user_agent and not self.stop:
+            try:
+                # Iterate through shared and thread-safe FileIO object
+                self.user_agent = self.wordlist.next()
+            except StopIteration:
+                # No more UA to process, stop this thread
+                self.stop = True
+            else:
+                # Debug used to catch race condition:
+                # COW mechanism or something else is causing race condition when we pass headers dict without doing deepcopy...
+                if DEBUG: print('Thread #{id}: {ua}'.format(id=self.index, ua=self.user_agent))
+                # if DEBUG: print('(bef) Thread #{id}: {ua}'.format(id=self.index, ua=self.user_agent))
+                # if DEBUG: print('(bef) Thread #{id}: {ua}'.format(id=self.index, ua=self.headers['User-Agent']))
 
-            if DEBUG: print('Thread #{id}: {ua}'.format(id=self.index, ua=self.user_agent))
+                # Update headers with custom User Agent (wordlist entry)
+                self.headers['User-Agent'] = self.user_agent
 
-            print(self.user_agent)
-            print(self.headers['User-Agent'])
-            self.headers['User-Agent'] = self.user_agent
+                # Get response from the server (fuzzed User Agent)
+                fuzzed_size, fuzzed_source = http_request(self.url, self.headers)
 
-            connection = httplib.HTTPConnection(urlparse(self.url).hostname)
-            connection.request(method='GET', url=self.url, body='', headers=self.headers)
-            reponse = connection.getresponse()
-            fuzzed = reponse.read().decode('utf-8')
-            fuzzed_source = re.sub(re.compile('.*' + re.escape(self.user_agent) + '.*\n'), '', fuzzed, count=1)
-            fuzzed_size = len(fuzzed) - len(self.user_agent)
-            connection.close()
+                # Remove reflected User Agent from source code
+                fuzzed_source = re.sub(re.compile('.*' + re.escape(self.headers['User-Agent']) + '.*\n'), '', fuzzed_source, count=1)
+                fuzzed_size = fuzzed_size - len(self.headers['User-Agent'].strip())
 
-            if self.standard_source != fuzzed_source and self.standard_size != fuzzed_size:
-            # if self.standard_size != fuzzed_size:
-                print('Thread #{id}: {url} (UA: {ua})'.format(id=self.index, url=self.url, ua=self.user_agent))
-                print(self.headers['User-Agent'])
-                print(self.user_agent)
+                # Check if the source code is different between response from standard and fuzzed User-Agent
+                if self.standard_source != fuzzed_source and self.standard_size != fuzzed_size:
+                    # Print User Agent that has made the source code change
+                    print('{url} (UA: {ua})'.format(url=self.url, ua=self.user_agent))
 
-            self.queue.task_done()
+                    # Another debug (may be used for race condition tracing purpose)
+                    # if DEBUG: print('(af) Thread #{id}: {ua}'.format(id=self.index, ua=self.user_agent))
+                    # if DEBUG: print('(af) Thread #{id}: {ua}'.format(id=self.index, ua=self.headers['User-Agent']))
+
+    def terminate(self):
+        # Terminate thread
+        if DEBUG: print('Terminating thread #{id}'.format(id=self.index))
+        self.stop = True        
+
+def http_request(url, headers):
+    """Simple HTTP request function with custom headers."""
+
+    # Prepare TCP connection for HTTP
+    connection = httplib.HTTPConnection(urlparse(url).hostname)
+
+    # Send GET request with custom headers to the HTTP server
+    connection.request(method='GET', url=url, body='', headers=headers)
+
+    # Get response from the HTTP server
+    response = connection.getresponse()
+
+    # Cleanup response and retrieve HTML source code
+    source = response.read().decode('utf-8')
+
+    # Close connection
+    connection.close()
+
+    # Return size of response and HTML source code
+    return (len(source), source)
 
 def fuzz_user_agent(url, wordlist_path, threads_count):
+    """User agent fuzzing."""
+
     # Default headers
     headers = {'User-Agent': STANDARD_UA, 'Accept': 'text/plain'}
 
-    connection = httplib.HTTPConnection(urlparse(url).hostname)
-    connection.request(method='GET', url=url, body='', headers=headers)
-    reponse = connection.getresponse()
+    # Get standard response from the server (default User Agent)
+    standard_size, standard_source = http_request(url, headers)
 
-    standard = reponse.read().decode('utf-8')
-    standard_source = re.sub(re.compile('.*' + re.escape(STANDARD_UA) + '.*\n'), '', standard, count=1)
-    standard_size = len(standard) - len(STANDARD_UA.strip())
+    # Remove reflected User Agent from source code
+    standard_source = re.sub(re.compile('.*' + re.escape(headers['User-Agent']) + '.*\n'), '', standard_source, count=1)
+    standard_size = standard_size - len(headers['User-Agent'].strip())
 
-    connection.close()
+    # Init wordlist
+    wordlist = Wordlist(wordlist_path)
 
+    # Prepare threads
+    threads = []
 
-    queue = Queue()
-
+    # Create n threads based on threads_count number
     for index in range(1, threads_count + 1):
-        worker = UAFuzz(index, queue, url, headers, standard_size, standard_source)
-        worker_thread = Thread(target=worker.run)
+        # Instantiate a new User Agent fuzzer (copy.deepcopy prevents COW mechanism)
+        worker = UAFuzz(index, wordlist, url, copy.deepcopy(headers), standard_size, standard_source)
+
+        # Create thread based on new UAFuzz instance
+        worker_thread = threading.Thread(target=worker.run)
+
+        # Daemonize thread (not used here since we're not using Queues)
         worker_thread.daemon = True
+
+        # Start the thread
         worker_thread.start()
 
+        # Add thread to threads list
+        threads += [{'thread': worker_thread, 'worker': worker}]
+
     try:
-        with codecs.open(wordlist_path, encoding='utf-8', errors='strict', buffering=1) as wordlist:
-            for word in wordlist:
-                queue.put(word.strip())
-        queue.join()
+        # Wait for threads to finish
+        for thread in threads:
+            thread['thread'].join()
     except KeyboardInterrupt:
+        # Close threads
+        for thread in threads:
+            thread['worker'].terminate()
+            thread['thread'].join()
+
+        # Close wordlist file descriptor
+        wordlist.close()
+
+        # Exit program
         sys.exit(1)
 
 def parse_args():
@@ -100,6 +175,7 @@ def parse_args():
     parser = argparse.ArgumentParser(
         description='JAWFT - Just another web fuzzing tool'
     )
+
     parser.add_argument('-w', '--wordlist',
                         type=str,
                         default='/usr/share/wordlists/rockyou.txt',
@@ -110,13 +186,15 @@ def parse_args():
                         help='URL to website')
     parser.add_argument('-t', '--threads',
                         type=int,
-                        default=2,
+                        default=1,
                         help='threads')
     parser.add_argument('-d', '--debug',
                         action='store_true',
                         default=False,
                         help='Debug')
+
     args = parser.parse_args()
+
     return args
 
 def main():
@@ -124,6 +202,8 @@ def main():
     try:
         # Arguments parsing
         args = parse_args()
+
+        # Overwrite DEBUG with user value (bool)
         global DEBUG
         DEBUG = args.debug
 
